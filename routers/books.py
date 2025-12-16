767933a46139449fedda230847680c78eae48a5e
Ekaterina Voronova <ekaterinaponurova@ya.ru>
2025-12-17 00:21:28 +0300

    feat: Create router with CRUD operations

---
+++ routers/books.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter
+
+from database import SessionDep
+from repository import BookRepository
+from schemas.books import SBook, SBookAdd
+
+router = APIRouter(prefix="/books", tags=["Книги"])
+
+
+@router.post("", response_model=SBook)
+async def create_book(
+    book: SBookAdd,
+    session: SessionDep,
+):
+    book_model = await BookRepository.add_one(book, session)
+    return book_model
+
+
+@router.get("", response_model=list[SBook])
+async def get_books(
+    session: SessionDep,
+):
+    books = await BookRepository.find_all(session)
+    return books
